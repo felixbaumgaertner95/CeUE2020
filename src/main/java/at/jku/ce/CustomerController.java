39b76fea1732cdaa5d2f9f3f7c507beacddeb69d
Felix Baumgärtner <felix.baumgaertner95@gmail.com>
2020-01-13 20:30:18 +0100

    Calling Post method after Login

--- src/main/java/at/jku/ce/CustomerController.java
+++ src/main/java/at/jku/ce/CustomerController.java
@@ -1,12 +1,24 @@
 package at.jku.ce;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
+@RestController
 public class CustomerController {
 
     @RequestMapping("/greeting")
     public void greeting(@RequestParam(value="name", defaultValue="World") String name) {
         System.out.println("Greeting");
+    }
+
+    @RequestMapping(value="/hello.html")
+    public void loginUser(@RequestParam(value="firstname", defaultValue="Hugo") String firstname, @RequestParam(value="lastname", defaultValue = "Hansi") String lastname)  {
+        try {
+            System.out.println("Post method called");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
